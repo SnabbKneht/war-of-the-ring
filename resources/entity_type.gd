787eb46887bcf8e42049a47b2b8f696ee742b647
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-12-29 16:11:49 +0100

    Add EntityType resource

---
+++ resources/entity_type.gd
@@ -0,0 +1,12 @@
+class_name EntityType
+extends Resource
+
+@export var id: String
+@export var side: Enums.Player
+@export var nation: Enums.Nation
+@export var combat_value: int
+@export var leadership_value: int
+@export var can_receive_damage: bool
+@export var can_be_reduced_to_regular: bool
+@export var must_be_in_army: bool
+@export var can_move_army: bool
---
+++ static-game-data-scripts/enums.gd
@@ -0,0 +1,18 @@
+class_name Enums
+
+enum Nation {
+	DWARVES,
+	ELVES,
+	NORTH,
+	ROHAN,
+	GONDOR,
+	ISENGARD,
+	SAURON,
+	EASTERLINGS,
+	NONE
+}
+
+enum Player {
+	FREE_PEOPLES,
+	SHADOW
+}
