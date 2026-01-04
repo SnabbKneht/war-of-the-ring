152c89eedf419587b3220d1775cf2a703ece4d91
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-04 14:18:38 +0100

    Add GamePaths

---
+++ game/data/game_paths.gd
@@ -0,0 +1,8 @@
+class_name GamePaths
+extends RefCounted
+
+
+const ARMY_SETUP_DIR: String = "res://game/data/resources/initial_setup/army_setup/"
+const REINFORCEMENT_SETUP_DIR: String = "res://game/data/resources/initial_setup/reinforcement_setup/"
+const PIECE_TYPE_DIR: String = "res://game/data/resources/pieces/"
+const REGION_DATA_DIR: String = "res://game/data/resources/regions/"
