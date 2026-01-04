bd15179fcec50b59f3564c4db4b523e41508fbb8
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-04 20:19:20 +0100

    Add GameStateFactory

---
+++ game/state/game_state_factory.gd
@@ -0,0 +1,8 @@
+class_name GameStateFactory
+extends RefCounted
+
+
+static func create() -> GameState:
+	var game_state: GameState = GameState.new()
+	game_state.map_state = MapStateFactory.create()
+	return game_state
