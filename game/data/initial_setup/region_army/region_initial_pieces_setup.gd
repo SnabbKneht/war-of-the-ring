74080c8c5e7f7c938fd544a1e69e34a81a0ab825
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-05 13:08:30 +0100

    Rename RegionInitialSetup to RegionInitialPiecesSetup

---
+++ game/data/initial_setup/region_army/region_initial_pieces_setup.gd
@@ -0,0 +1,6 @@
+class_name RegionInitialPiecesSetup
+extends Resource
+
+
+@export var region_id: int
+@export var pieces: Array[PieceType]
--- game/data/initial_setup/region_army/region_initial_setup.gd
+++
@@ -1,6 +0,0 @@
-class_name RegionInitialSetup
-extends Resource
-
-
-@export var region_id: int
-@export var pieces: Array[PieceType]
