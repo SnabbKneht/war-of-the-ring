4ef2aa64cbc8904c418357c3fcf348ddf5cffe7f
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-05 21:13:07 +0100

    Delete GamePaths and RegionDatabase

--- game/data/paths/game_paths.gd
+++
@@ -1,9 +0,0 @@
-class_name GamePaths
-extends RefCounted
-
-
-const REGION_INITIAL_ARMY_SETUP_DIR: String = "res://game/data/initial_setup/region_army/region_initial_pieces_setup_resources/"
-const REINFORCEMENT_INITIAL_SETUP_DIR: String = "res://game/data/initial_setup/reinforcement/reinforcement_initial_setup_resources/"
-const PIECE_TYPE_DIR: String = "res://game/data/pieces/piece_type_resources/"
-const REGION_DATA_DIR: String = "res://game/data/regions/region_data_resources/"
-const PIECE_INVENTORY_RES: String = "res://game/data/inventory/base_game_piece_inventory.tres"
