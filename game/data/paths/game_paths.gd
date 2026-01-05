552627917c04f0794de6f32d76f3e07e7f6c9218
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-05 13:23:47 +0100

    Update paths

--- game/data/paths/game_paths.gd
+++ game/data/paths/game_paths.gd
@@ -2,8 +2,8 @@ class_name GamePaths
 extends RefCounted
 
 
-const ARMY_SETUP_DIR: String = "res://game/data/initial_setup/region_army/region_initial_setup_resources/"
-const REINFORCEMENT_SETUP_DIR: String = "res://game/data/initial_setup/reinforcement/reinforcement_initial_setup_resources/"
+const REGION_INITIAL_ARMY_SETUP_DIR: String = "res://game/data/initial_setup/region_army/region_initial_pieces_setup_resources/"
+const REINFORCEMENT_INITIAL_SETUP_DIR: String = "res://game/data/initial_setup/reinforcement/reinforcement_initial_setup_resources/"
 const PIECE_TYPE_DIR: String = "res://game/data/pieces/piece_type_resources/"
 const REGION_DATA_DIR: String = "res://game/data/regions/region_data_resources/"
 const PIECE_INVENTORY_RES: String = "res://game/data/inventory/base_game_piece_inventory.tres"
