7581c3d0fdc4a86ff058e25f23d4fe7264759b13
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-06 16:02:22 +0100

    Rework piece pool

--- game/state/game/game_state_factory.gd
+++ game/state/game/game_state_factory.gd
@@ -5,5 +5,4 @@ extends RefCounted
 static func create() -> GameState:
 	var game_state: GameState = GameState.new()
 	game_state.map_state = MapStateFactory.create()
-	game_state.piece_pool = PiecePoolFactory.create()
 	return game_state
--- game/state/pieces/piece_pool.gd
+++ game/state/pieces/piece_pool.gd
@@ -3,7 +3,15 @@ extends RefCounted
 
 
 var _pieces: Dictionary[int, Piece] = {}
+var _next_id: int = 1
 
 
 func get_all_pieces() -> Array[Piece]:
 	return _pieces.values()
+
+
+func create_new_piece(piece_type: PieceType) -> Piece:
+	var new_piece: Piece = Piece.new(_next_id, piece_type)
+	_pieces[_next_id] = new_piece
+	_next_id += 1
+	return new_piece
