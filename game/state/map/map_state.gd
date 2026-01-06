a6df69f868222480d6f1de89e2a043e0e5282597
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-06 16:04:31 +0100

    Expand MapState

--- game/state/map/map_state.gd
+++ game/state/map/map_state.gd
@@ -2,4 +2,13 @@ class_name MapState
 extends RefCounted
 
 
-var region_states: Dictionary[int, RegionState] = {}
+var _region_states: Dictionary[int, RegionState] = {}
+
+
+func get_region_state_by_id(id: int) -> RegionState:
+	assert(_region_states.keys().has(id))
+	return _region_states[id]
+
+
+func add_pieces_to_region(id: int, pieces: Array[Piece]) -> void:
+	get_region_state_by_id(id).add_pieces(pieces)
--- game/state/map/map_state_factory.gd
+++ game/state/map/map_state_factory.gd
@@ -5,5 +5,5 @@ extends RefCounted
 static func create() -> MapState:
 	var map_state: MapState = MapState.new()
 	for region_data: RegionData in GameData.get_all_region_data():
-		map_state.region_states[region_data.id] = RegionState.new(region_data)
+		map_state._region_states[region_data.id] = RegionState.new(region_data)
 	return map_state
