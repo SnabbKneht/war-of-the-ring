b7e686c65b2c1bd9800c5699bc79356a69d9c35b
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-06 14:05:00 +0100

    Make GameData an autoload

--- game/data/game/game_data.gd
+++ game/data/game/game_data.gd
@@ -1,24 +1,39 @@
-class_name GameData
-extends RefCounted
+extends Node
 
 
 const GAME_DATA_PATH: String = "res://game/data/game/game_data_resource.tres"
 
 
-static var _data: GameDataResource = preload(GAME_DATA_PATH)
+var _data: GameDataResource = preload(GAME_DATA_PATH)
 
 
-static func get_all_region_data() -> Array[RegionData]:
+var _region_by_id: Dictionary[int, RegionData] = {}
+
+
+func _init() -> void:
+	_init_region_dictionary()
+
+
+func get_all_region_data() -> Array[RegionData]:
 	return _data.all_region_data
 
 
-static func get_all_region_initial_pieces_setups() -> Array[RegionInitialPiecesSetup]:
+func get_region_by_id(id: int) -> RegionData:
+	return _region_by_id[id]
+
+
+func get_all_region_initial_pieces_setups() -> Array[RegionInitialPiecesSetup]:
 	return _data.all_region_initial_pieces_setups
 
 
-static func get_all_reinforcement_initial_setups() -> Array[ReinforcementInitialSetup]:
+func get_all_reinforcement_initial_setups() -> Array[ReinforcementInitialSetup]:
 	return _data.all_reinforcement_initial_setups
 
 
-static func get_base_game_piece_inventory() -> PieceInventory:
+func get_base_game_piece_inventory() -> PieceInventory:
 	return _data.base_game_piece_inventory
+
+
+func _init_region_dictionary() -> void:
+	for region_data: RegionData in get_all_region_data():
+		_region_by_id[region_data.id] = region_data
