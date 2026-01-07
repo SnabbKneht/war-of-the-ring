a55db5f98f68ef9eefc43987620a191addbeae3f
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-07 15:03:32 +0100

    Make MapValidator's helper functions private

--- tests/map_validator.gd
+++ tests/map_validator.gd
@@ -3,40 +3,40 @@ extends RefCounted
 
 
 static func validate_all() -> void:
-	validate_region_count()
-	validate_region_ids()
-	validate_no_null_regions()
-	validate_no_null_neighbors()
-	validate_no_self_neighbors()
-	validate_mutual_neighbors()
+	_validate_region_count()
+	_validate_region_ids()
+	_validate_no_null_regions()
+	_validate_no_null_neighbors()
+	_validate_no_self_neighbors()
+	_validate_mutual_neighbors()
 
 
-static func validate_region_count() -> void:
+static func _validate_region_count() -> void:
 	assert(GameData.get_all_region_data().size() == 105, "Region count mismatch: is " + str(GameData.get_all_region_data().size()) + ", should be 105.")
 
 
-static func validate_region_ids() -> void:
+static func _validate_region_ids() -> void:
 	for id: int in range(1, 106):
 		assert(GameData.get_all_region_ids().has(id), "Missing region id: " + str(id))
 
 
-static func validate_no_null_regions() -> void:
+static func _validate_no_null_regions() -> void:
 	for id: int in range(1, 106):
 		assert(GameData.get_region_by_id(id) != null, "Region " + str(id) + " is null")
 
 
-static func validate_no_null_neighbors() -> void:
+static func _validate_no_null_neighbors() -> void:
 	for region_data: RegionData in GameData.get_all_region_data():
 		for neighbor_id: int in region_data.neighbors:
 			assert(GameData.get_region_by_id(neighbor_id) != null, "Region " + str(region_data.id) + " has a null neighbor.")
 
 
-static func validate_no_self_neighbors() -> void:
+static func _validate_no_self_neighbors() -> void:
 	for region_data: RegionData in GameData.get_all_region_data():
 		assert(region_data.id not in region_data.neighbors, "Region " + str(region_data.id) + " is its own neighbor.")
 
 
-static func validate_mutual_neighbors() -> void:
+static func _validate_mutual_neighbors() -> void:
 	for region: RegionData in GameData.get_all_region_data():
 		for neighbor_id: int in region.neighbors:
 			var neighbor: RegionData = GameData.get_region_by_id(neighbor_id)
