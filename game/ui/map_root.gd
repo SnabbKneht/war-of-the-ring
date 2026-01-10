91f4feeb7aaa3d8c3032c9a61369855a3f183a49
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-10 22:03:43 +0100

    Fix sprite node path

--- game/ui/map_root.gd
+++ game/ui/map_root.gd
@@ -2,7 +2,7 @@ class_name MapRoot
 extends Node2D
 
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var color_sprite: Sprite2D = $RegionColorMap
 
 
 func _ready() -> void:
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		var color_clicked: Color = sprite_2d.texture.get_image().get_pixelv(event.position)
+		var color_clicked: Color = color_sprite.texture.get_image().get_pixelv(event.position)
 		if color_clicked in UIData.get_all_region_colors():
 			var region_id: int = UIData.get_region_id_by_color(color_clicked)
 			print(GameData.get_region_by_id(region_id).name)
