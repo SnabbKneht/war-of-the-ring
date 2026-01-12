9ce892390e551cd7c7c0338fa813e957ee90460b
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-12 11:33:07 +0100

    Fix region detection to work with zoom and panning

--- game/ui/map/map_root.gd
+++ game/ui/map/map_root.gd
@@ -16,7 +16,11 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		var color_clicked: Color = color_sprite.texture.get_image().get_pixelv(event.position)
+		var image_pos: Vector2 = color_sprite.to_local(get_global_mouse_position()) + color_sprite.texture.get_size() * 0.5
+		var image_bounds: Rect2 = Rect2(Vector2.ZERO, color_sprite.texture.get_size())
+		if(not image_bounds.has_point(image_pos)):
+			return
+		var color_clicked: Color = color_sprite.texture.get_image().get_pixelv(image_pos)
 		if color_clicked in UIData.get_all_region_colors():
 			var region_id: int = UIData.get_region_id_by_color(color_clicked)
 			print(game_state.map_state.get_region_state_by_id(region_id).get_info())
