15c86621c09e805bff71e9db352d0893464687ba
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-14 17:38:04 +0100

    Expand RegionState

--- game/state/map/region_state.gd
+++ game/state/map/region_state.gd
@@ -2,6 +2,9 @@ class_name RegionState
 extends RefCounted
 
 
+signal changed
+
+
 func _init(region_data: RegionData) -> void:
 	self.region_data = region_data
 	controlled_by = region_data.side
@@ -16,18 +19,35 @@ var is_conquered: bool
 
 func add_piece(piece: Piece) -> void:
 	_pieces.append(piece)
+	changed.emit()
 
 
 func add_pieces(pieces: Array[Piece]) -> void:
 	_pieces.append_array(pieces)
+	changed.emit()
 
 
 func remove_pieces(pieces: Array[Piece]) -> void:
 	for p: Piece in pieces:
 		assert(_pieces.has(p))
 		_pieces.erase(p)
+	changed.emit()
 
 
-func get_info() -> String:
+func get_total_combat_value() -> int:
+	var result: int = 0
+	for piece: Piece in _pieces:
+		result += piece.combat_value
+	return result
+
+
+func get_total_leadership_value() -> int:
+	var result: int = 0
+	for piece: Piece in _pieces:
+		result += piece.leadership_value
+	return result
+
+
+func _to_string() -> String:
 	var info: String = region_data.name + ": " + str(_pieces)
 	return info
