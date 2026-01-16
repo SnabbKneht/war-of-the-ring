f645c8e77347af39c3640dac7f6330403bd73286
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-16 14:07:17 +0100

    Add get_nation_state method to PoliticalState

--- game/state/politics/political_state.gd
+++ game/state/politics/political_state.gd
@@ -13,3 +13,8 @@ func get_nation_step(nation: Enums.Nation) -> int:
 func is_nation_active(nation: Enums.Nation) -> bool:
 	assert(nation in _nation_to_state.keys())
 	return _nation_to_state[nation].is_active()
+
+
+func get_nation_state(nation: Enums.Nation) -> NationState:
+	assert(nation in _nation_to_state.keys())
+	return _nation_to_state[nation]
