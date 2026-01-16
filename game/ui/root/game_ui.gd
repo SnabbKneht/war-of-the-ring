e58e0474e2b079d99ddba25a3d34b84888f77856
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-16 15:09:43 +0100

    Add PoliticalTrackView to GameUI

--- game/ui/root/game_ui.gd
+++ game/ui/root/game_ui.gd
@@ -3,7 +3,9 @@ extends Node
 
 
 @onready var map_view: MapView = $MapView
+@onready var political_track_view: PoliticalTrackView = $HUD/PoliticalTrackView
 
 
 func set_game_state(game_state: GameState) -> void:
 	map_view.set_map_state(game_state.map_state)
+	political_track_view.set_political_state(game_state.political_state)
