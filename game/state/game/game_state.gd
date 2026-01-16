b5f4de61da59b543409ec850dd73a94c40261b46
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-16 12:04:07 +0100

    Add political state to game state

--- game/state/game/game_state.gd
+++ game/state/game/game_state.gd
@@ -6,3 +6,4 @@ var map_state: MapState
 var piece_pool: PiecePool = PiecePool.new()
 var reinforcements: Reinforcements = Reinforcements.new()
 var casualties: Casualties = Casualties.new()
+var political_state: PoliticalState
--- game/state/game/game_state_factory.gd
+++ game/state/game/game_state_factory.gd
@@ -6,4 +6,5 @@ static func create() -> GameState:
 	var game_state: GameState = GameState.new()
 	game_state.map_state = MapStateFactory.create()
 	PiecesInitialPopulator.populate(game_state)
+	game_state.political_state = PoliticalStateFactory.create()
 	return game_state
