464f004a1da02e9be5e4b32ad59566aafafc024e
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-17 09:42:58 +0100

    Change nation track UI to progress bar

--- game/ui/politics/nation_track_view.gd
+++ game/ui/politics/nation_track_view.gd
@@ -2,10 +2,9 @@ class_name NationTrackView
 extends HBoxContainer
 
 
-@onready var icon_1: TextureRect = $Square1/NationIcon
-@onready var icon_2: TextureRect = $Square2/NationIcon2
-@onready var icon_3: TextureRect = $Square3/NationIcon3
-@onready var icon_4: TextureRect = $Square4/NationIcon4
+@onready var nation_icon: TextureRect = $NationIcon
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var war_label: Label = $ProgressBar/WarLabel
 
 
 @export var nation: Enums.Nation
@@ -19,36 +18,24 @@ func set_nation_state(nation_state: NationState) -> void:
 
 
 func _refresh() -> void:
-	_refresh_icon_textures()
-	_refresh_icon_visibility()
+	_refresh_icon_texture()
+	_refresh_progress_bar()
+	_refresh_war_label()
 
 
-func _get_texture() -> Texture2D:
+func _refresh_icon_texture() -> void:
 	if _nation_state.is_active():
-		return UIData.get_active_nation_icon(_nation_state.get_nation())
+		nation_icon.texture = UIData.get_active_nation_icon(_nation_state.get_nation())
 	else:
-		return UIData.get_nation_icon(_nation_state.get_nation())
+		nation_icon.texture = UIData.get_nation_icon(_nation_state.get_nation())
 
 
-func _refresh_icon_textures() -> void:
-	var texture: Texture2D = _get_texture()
-	icon_1.texture = texture
-	icon_2.texture = texture
-	icon_3.texture = texture
-	icon_4.texture = texture
+func _refresh_progress_bar() -> void:
+	progress_bar.value = _nation_state.get_step()
 
 
-func _refresh_icon_visibility() -> void:
-	icon_1.hide()
-	icon_2.hide()
-	icon_3.hide()
-	icon_4.hide()
-	match _nation_state.get_step():
-		1:
-			icon_1.show()
-		2:
-			icon_2.show()
-		3:
-			icon_3.show()
-		4:
-			icon_4.show()
+func _refresh_war_label() -> void:
+	if _nation_state.is_at_war():
+		war_label.show()
+	else:
+		war_label.hide()
