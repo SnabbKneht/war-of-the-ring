d7115aefcea84b2cce39613589b3654969cb9982
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-19 12:26:21 +0100

    Add is_nation_at_war method to PoliticalState

--- game/state/politics/political_state.gd
+++ game/state/politics/political_state.gd
@@ -10,6 +10,10 @@ func get_nation_step(nation: Enums.Nation) -> int:
 	return _nation_to_state[nation].get_step()
 
 
+func is_nation_at_war(nation: Enums.Nation) -> bool:
+	return get_nation_state(nation).is_at_war()
+
+
 func is_nation_active(nation: Enums.Nation) -> bool:
 	assert(nation in _nation_to_state.keys())
 	return _nation_to_state[nation].is_active()
