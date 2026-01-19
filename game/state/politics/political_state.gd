4fabf55e420b70dad945da6b86e92ee7fcf999c1
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-19 12:23:19 +0100

    Add advance and move back methods to political state

--- game/state/politics/political_state.gd
+++ game/state/politics/political_state.gd
@@ -18,3 +18,11 @@ func is_nation_active(nation: Enums.Nation) -> bool:
 func get_nation_state(nation: Enums.Nation) -> NationState:
 	assert(nation in _nation_to_state.keys())
 	return _nation_to_state[nation]
+
+
+func advance_nation(nation: Enums.Nation) -> void:
+	get_nation_state(nation).advance()
+
+
+func move_back_nation(nation: Enums.Nation) -> void:
+	get_nation_state(nation).move_back()
