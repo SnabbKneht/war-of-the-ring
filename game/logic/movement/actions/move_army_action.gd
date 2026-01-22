8f4d400e79211e4ce62b52051f539916ce81f13e
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-22 21:22:21 +0100

    Add empty MovementLogic and MoveArmyAction classes

---
+++ game/logic/movement/actions/move_army_action.gd
@@ -0,0 +1,25 @@
+class_name MoveArmyAction
+extends GameAction
+
+
+var _piece_ids: Array[int]
+var _from_region_id: int
+var _to_region_id: int
+
+
+func _init(p_piece_ids: Array[int], p_from_region_id: int, p_to_region_id: int) -> void:
+	_piece_ids = p_piece_ids
+	_from_region_id = p_from_region_id
+	_to_region_id = p_to_region_id
+
+
+func can_apply(game_state: GameState) -> bool:
+	return true
+
+
+func apply(game_state: GameState) -> void:
+	pass
+
+
+func undo(game_state: GameState) -> void:
+	pass
