a543e8482ffbf9b672b0a27be93d599e3c1c3901
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-24 11:39:09 +0100

    Add get_piece method to PiecePool

--- game/state/pieces/piece_pool.gd
+++ game/state/pieces/piece_pool.gd
@@ -5,5 +5,10 @@ extends RefCounted
 var _pieces: Dictionary[StringName, Piece] = {}
 
 
+func get_piece(id: StringName) -> Piece:
+	assert(id in _pieces.keys())
+	return _pieces[id]
+
+
 func get_all_pieces() -> Array[Piece]:
 	return _pieces.values()
