522308a1f3c6a5b17ed4e18f6d7f127693e29c71
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-01-26 13:24:26 +0100

    Allow piece to always enter a region belonging to no nation

--- game/logic/movement/movement_logic.gd
+++ game/logic/movement/movement_logic.gd
@@ -7,15 +7,15 @@ static func can_move_army(game_state: GameState, piece_ids: Array[StringName], f
 
 
 static func _can_piece_enter_nation(game_state: GameState, piece_id: StringName, nation: Enums.Nation) -> bool:
+	if nation == Enums.Nation.NONE:
+		return true
 	var piece: Piece = game_state.piece_pool.get_piece(piece_id)
 	if not piece.is_army_unit():
-		return true
-	elif game_state.political_state.is_nation_at_war(piece.get_nation()):
 		return true
 	elif piece.get_nation() == nation:
 		return true
 	else:
-		return false
+		return game_state.political_state.is_nation_at_war(piece.get_nation())
 
 
 static func _can_all_pieces_enter_nation(game_state: GameState, piece_ids: Array[StringName], nation: Enums.Nation) -> bool:
