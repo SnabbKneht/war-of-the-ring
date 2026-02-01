9a82596f62ee9d9f0ecf51be1c912ac77b200292
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2026-02-01 15:29:24 +0100

    Separate DefaultHUD from DefaultMode

--- game/controller/interaction_mode/default/default_mode.gd
+++ game/controller/interaction_mode/default/default_mode.gd
@@ -6,21 +6,19 @@ signal advance_nation_requested(nation: Enums.Nation)
 signal move_back_nation_requested(nation: Enums.Nation)
 
 
-@onready var political_track_view: PoliticalTrackView = $HUD/PoliticalTrackView
-@onready var region_details_view: RegionDetailsView = $HUD/RegionDetailsView
+@onready var _default_hud: DefaultHUD = $DefaultHUD
 
 
 func _ready() -> void:
-	political_track_view.advance_nation_requested.connect(advance_nation_requested.emit)
-	political_track_view.move_back_nation_requested.connect(move_back_nation_requested.emit)
-	region_details_view.neighbor_highlight_requested.connect(_on_neighbor_highlight_requested)
-	region_details_view.neighbor_highlight_cancelled.connect(_on_neighbor_highlight_cancelled)
+	_default_hud.advance_nation_requested.connect(advance_nation_requested.emit)
+	_default_hud.move_back_nation_requested.connect(move_back_nation_requested.emit)
+	_default_hud.neighbor_highlight_requested.connect(_on_neighbor_highlight_requested)
+	_default_hud.neighbor_highlight_cancelled.connect(_on_neighbor_highlight_cancelled)
 
 
 func enter(p_game_state: GameState, p_game_ui: GameUI) -> void:
 	super(p_game_state, p_game_ui)
-	political_track_view.set_game_state(_game_state)
-	political_track_view.set_political_state(_game_state.political_state)
+	_default_hud.set_game_state(_game_state)
 	_game_ui.region_hovered.connect(_on_region_hovered)
 	_game_ui.region_selected.connect(_on_region_selected)
 	_game_ui.region_deselected.connect(_on_region_deselected)
@@ -44,25 +42,16 @@ func _on_neighbor_highlight_cancelled(_region_id: int) -> void:
 
 
 func _on_region_selected(region_id: int) -> void:
-	_show_region_details(_game_state.map_state.get_region_state_by_id(region_id))
+	_default_hud.show_region_details(_game_state.map_state.get_region_state_by_id(region_id))
 	_game_ui.clear_region_overlay(UIEnums.RegionOverlay.SELECTION)
 	_game_ui.add_region_overlay([region_id], UIEnums.RegionOverlay.SELECTION)
 
 
 func _on_region_deselected() -> void:
-	_hide_region_details()
+	_default_hud.hide_region_details()
 	_game_ui.clear_region_overlay(UIEnums.RegionOverlay.SELECTION)
 
 
 func _on_region_hovered(region_id: int) -> void:
 	_game_ui.clear_region_overlay(UIEnums.RegionOverlay.HIGHLIGHT)
 	_game_ui.add_region_overlay([region_id], UIEnums.RegionOverlay.HIGHLIGHT)
-
-
-func _show_region_details(region_state: RegionState) -> void:
-	region_details_view.set_region_state(region_state)
-	region_details_view.show()
-
-
-func _hide_region_details() -> void:
-	region_details_view.hide()
---
+++ game/ui/interaction_modes/default/default_hud.gd
@@ -0,0 +1,37 @@
+class_name DefaultHUD
+extends CanvasLayer
+
+
+signal advance_nation_requested(nation: Enums.Nation)
+signal move_back_nation_requested(nation: Enums.Nation)
+signal neighbor_highlight_requested(region_id: int)
+signal neighbor_highlight_cancelled(region_id: int)
+
+
+@onready var _political_track_view: PoliticalTrackView = $PoliticalTrackView
+@onready var _region_details_view: RegionDetailsView = $RegionDetailsView
+
+
+var _game_state: GameState
+
+
+func _ready() -> void:
+	_political_track_view.advance_nation_requested.connect(advance_nation_requested.emit)
+	_political_track_view.move_back_nation_requested.connect(move_back_nation_requested.emit)
+	_region_details_view.neighbor_highlight_requested.connect(neighbor_highlight_requested.emit)
+	_region_details_view.neighbor_highlight_cancelled.connect(neighbor_highlight_cancelled.emit)
+
+
+func set_game_state(p_game_state: GameState) -> void:
+	_game_state = p_game_state
+	_political_track_view.set_game_state(_game_state)
+	_political_track_view.set_political_state(_game_state.political_state)
+
+
+func show_region_details(region_state: RegionState) -> void:
+	_region_details_view.set_region_state(region_state)
+	_region_details_view.show()
+
+
+func hide_region_details() -> void:
+	_region_details_view.hide()
